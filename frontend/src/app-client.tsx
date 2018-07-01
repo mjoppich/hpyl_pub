2e30ab2d32b68f0390f5b54f42b734698e590287
Markus Joppich <github@mjoppich.net>
2018-07-01 21:59:04 +0200

    server compatibility

--- frontend/src/app-client.tsx
+++ frontend/src/app-client.tsx
@@ -4,9 +4,11 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MainApp } from './pages/App';
+import config from './main_pages/config';
+
 
 const AppClient = () => 
-<Router>
+<Router basename={'/'+config.restFolder}>
   <MainApp/>
 </Router>
 ;
